64a184c39731e67faa48042426f5f13fdc55f737
Andrew Hoang <andrewhoang7@gmail.com>
2022-02-17 08:53:08 -0600

    adding global variable previewWeather, adding extension on Double

---
+++ Weather SwiftUI/Weather SwiftUI/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Weather SwiftUI
+//
+//  Created by Andrew Hoang on 2/17/22.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        String(format: "%.0f", self)
+    }
+}
