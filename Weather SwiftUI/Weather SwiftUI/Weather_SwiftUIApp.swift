21f8f05dfe1245ce118fa5d46c45c70b414ae99d
Andrew Hoang <andrewhoang7@gmail.com>
2022-02-16 20:10:17 -0600

    Weather SwiftUI app project creation

---
+++ Weather SwiftUI/Weather SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather SwiftUI
+//
+//  Created by Andrew Hoang on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
