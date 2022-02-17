461b7f8ed96c6dfb8696ceaa782974ba302e26ac
Andrew Hoang <andrewhoang7@gmail.com>
2022-02-17 09:16:58 -0600

    adding code to WeatherRow component
    min temp, max temp, wind speed, humidity

--- Weather SwiftUI/Weather SwiftUI/Views/WeatherView.swift
+++ Weather SwiftUI/Weather SwiftUI/Views/WeatherView.swift
@@ -57,6 +57,33 @@ struct WeatherView: View {
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min Temp", value: (weather.main.temp_min.roundDouble() + "º"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max Temp", value: (weather.main.temp_max.roundDouble() + "º"))
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind Speed", value: (weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: (weather.main.humidity.roundDouble() + "%"))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.587, saturation: 0.938, brightness: 0.493))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.587, saturation: 0.938, brightness: 0.493))
