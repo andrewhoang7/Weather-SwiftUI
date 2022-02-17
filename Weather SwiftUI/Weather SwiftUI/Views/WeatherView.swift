afe0236f165838150c3ce594c70eeb481c024193
Andrew Hoang <andrewhoang7@gmail.com>
2022-02-17 08:27:16 -0600

    adding weatherView, load f(n), and weatherData mock json
    also adding breakpoint file (.xcbptlist)

---
+++ Weather SwiftUI/Weather SwiftUI/Views/WeatherView.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherView.swift
+//  Weather SwiftUI
+//
+//  Created by Andrew Hoang on 2/17/22.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    
+    var weather: ResponseBody
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
