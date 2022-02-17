2fc3dbb706af74fe23f7e845ed179462a5c7fd26
Andrew Hoang <andrewhoang7@gmail.com>
2022-02-16 20:53:46 -0600

    create WelcomeView

--- Weather SwiftUI/Weather SwiftUI/Views/ContentView.swift
+++ Weather SwiftUI/Weather SwiftUI/Views/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            WelcomeView()
+                .environmentObject(locationManager)
+        }
+        .background(Color(hue: 0.587, saturation: 0.807, brightness: 0.872))
     }
 }
 
